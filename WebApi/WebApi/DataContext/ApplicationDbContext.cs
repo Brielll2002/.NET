266a0e7ecaee6c3ba47debfd7ada9db73284d81c
Gabriel Rodrigues <gabrielrodrigues62902@gmail.com>
2024-06-03 16:31:29 -0300

    Primeiro commit

---
+++ WebApi/WebApi/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {}
+
+        public DbSet<FuncionarioModel> Funcionarios { get; set; }
+
+        internal void SaveChanges(object novoFuncionario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApi/WebApi/Models/ServiceResponse.cs
@@ -0,0 +1,20 @@
+﻿
+namespace WebApi.Models
+{
+    public class ServiceResponse<T>
+    {
+        public T? Dados { get; set; }
+        public string Mensagem { get; set; } = string.Empty;
+        public bool Sucesso { get; set; } = true;
+
+        public static implicit operator ServiceResponse<T>(ServiceResponse<FuncionarioModel> v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static explicit operator ServiceResponse<T>(ServiceResponse<List<FuncionarioModel>> v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
